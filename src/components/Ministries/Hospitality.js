a95d74696bd5121f31229e2d5ab875c1105bd7ac
Rouncer27 <trevor@switchbackcreative.ca>
2020-05-26 14:51:33 -0600

    more styling of the ministries page

--- src/components/Ministries/Hospitality.js
+++ src/components/Ministries/Hospitality.js
@@ -1,11 +1,81 @@
 import React from "react"
-import Img from "gatsby-image"
+import BGImg from "gatsby-background-image"
+import styled from "styled-components"
+import { colors, H2LatoGold, B1OpenSansWhite } from "../../styles/helpers"
+
+const HospitalitySection = styled.section`
+  position: relative;
+  margin-bottom: 5rem;
+  background-color: ${colors.colorPrimary};
+
+  @media (min-width: 1025px) {
+    margin-bottom: 10rem;
+  }
+
+  .wrapper {
+    position: relative;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    width: 100%;
+    z-index: 5;
+  }
+
+  .content {
+    width: 100%;
+    padding: 2rem;
+
+    @media (min-width: 768px) {
+      width: calc(55% - 3rem);
+      margin-right: 3rem;
+      margin-left: auto;
+    }
+
+    @media (min-width: 1024px) {
+      width: calc(60% - 5.5rem);
+      max-width: 65rem;
+      margin-right: 5.5rem;
+      margin-left: auto;
+      padding: 8.5rem 0;
+    }
+
+    h2 {
+      ${H2LatoGold};
+    }
+
+    p {
+      ${B1OpenSansWhite};
+    }
+  }
+
+  .image {
+    position: relative;
+    width: 100%;
+
+    @media (min-width: 768px) {
+      width: calc(45%);
+    }
+
+    @media (min-width: 1024px) {
+      width: calc(40%);
+    }
+
+    &__background {
+      position: absolute !important;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      z-index: 5;
+    }
+  }
+`
 
 const Hospitality = ({ hospitality }) => {
   return (
-    <div>
-      <div>
-        <div>
+    <HospitalitySection>
+      <div className="wrapper">
+        <div className="content">
           <div>
             <h2>{hospitality.acf._wfc_hos_title}</h2>
           </div>
@@ -15,16 +85,17 @@ const Hospitality = ({ hospitality }) => {
             }}
           />
         </div>
-        <div>
-          <Img
+        <div className="image">
+          <BGImg
             fluid={
               hospitality.acf._wfc_hos_image.localFile.childImageSharp.fluid
             }
-            alt={hospitality.acf._wfc_hos_image.alt_text}
+            className="image__background"
+            tag="div"
           />
         </div>
       </div>
-    </div>
+    </HospitalitySection>
   )
 }
 
