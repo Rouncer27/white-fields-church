8f096834176796343eac4b59630f3c72cd005aba
Rouncer27 <trevor@switchbackcreative.ca>
2020-05-26 10:18:26 -0600

    ministries hero image

--- src/templates/ministries.js
+++ src/templates/ministries.js
@@ -3,14 +3,36 @@ import { graphql } from "gatsby"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import MinistriesHero from "../components/Ministries/MinistriesHero"
 
-const Ministries = () => {
+const Ministries = props => {
+  const { hero } = props.data
   return (
     <Layout>
-      <SEO />
-      <h1>Ministries</h1>
+      <SEO title="Ministries Page" />
+      <MinistriesHero hero={hero} />
     </Layout>
   )
 }
+
+export const ministriesQuery = graphql`
+  query ministriesPage($id: Int!) {
+    hero: wordpressPage(wordpress_id: { eq: $id }) {
+      acf {
+        _wfc_min_hero_img {
+          alt_text
+          localFile {
+            childImageSharp {
+              fluid(maxWidth: 2000) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+        }
+        _wfc_min_hero_title
+      }
+    }
+  }
+`
 
 export default Ministries
