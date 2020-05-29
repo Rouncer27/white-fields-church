d0f270874dca1cf25039ffbc60fe7065391d8e03
Rouncer27 <trevor@switchbackcreative.ca>
2020-05-29 09:21:08 -0600

    work on the series items

--- src/components/Messages/Series.js
+++ src/components/Messages/Series.js
@@ -1,21 +1,18 @@
 import React from "react"
 import styled from "styled-components"
-import Img from "gatsby-image"
-import { Link } from "gatsby"
-import {
-  standardWrapper,
-  B2OpenSansWhite,
-  buttonOneClear,
-} from "../../styles/helpers"
+import { medWrapper } from "../../styles/helpers"
 
 import BrushStrokeSeven from "../Graphics/BrushStrokeSeven"
 import BrushStrokeFour from "../Graphics/BrushStrokeFour"
+import SeriesItem from "./SeriesItem"
 
 const SeriesSection = styled.section`
   position: relative;
   overflow: hidden;
+
   .wrapper {
-    ${standardWrapper};
+    ${medWrapper};
+    align-items: center;
   }
 
   .graphic {
@@ -62,95 +59,13 @@ const SeriesSection = styled.section`
   }
 `
 
-const SeriesItem = styled.div`
-  position: relative;
-  width: 100%;
-
-  @media (min-width: 768px) {
-    width: calc(50%);
-  }
-
-  @media (min-width: 1025px) {
-    width: calc(50% - 4rem);
-    margin: 2rem;
-  }
-
-  .image {
-    width: 100%;
-  }
-
-  .title {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    padding: 2rem;
-
-    &__content {
-      position: relative;
-      z-index: 10;
-
-      h2 {
-        ${B2OpenSansWhite};
-      }
-    }
-
-    &__button {
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      z-index: 10;
-
-      button {
-        ${buttonOneClear};
-      }
-    }
-
-    &__background {
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      background-color: rgba(55, 93, 119, 0.5);
-      z-index: 5;
-    }
-  }
-`
-
 const Series = ({ series }) => {
-  console.log(series)
   return (
     <SeriesSection>
       <div className="wrapper">
-        {series.edges.map(item => {
-          return (
-            <SeriesItem key={item.node.wordpress_id}>
-              <Link to={`/series/${item.node.slug}`}>
-                <div className="image">
-                  <Img
-                    fluid={
-                      item.node.acf._wfc_mescat_featured_image.localFile
-                        .childImageSharp.fluid
-                    }
-                    alt={item.node.name}
-                  />
-                </div>
-                <div className="title">
-                  <div className="title__content">
-                    <h2>{item.node.name}</h2>
-                  </div>
-                  <div className="title__button">
-                    <button>View Series</button>
-                  </div>
-                  <div className="title__background" />
-                </div>
-              </Link>
-            </SeriesItem>
-          )
-        })}
+        {series.edges.map(item => (
+          <SeriesItem key={item.node.wordpress_id} item={item} />
+        ))}
       </div>
       <div className="graphic">
         <BrushStrokeSeven />
---
+++ src/components/Messages/SeriesItem.js
@@ -0,0 +1,107 @@
+import React from "react"
+import styled from "styled-components"
+import Img from "gatsby-image"
+import { Link } from "gatsby"
+import { B2OpenSansWhite, buttonOneClear } from "../../styles/helpers"
+
+const SeriesItemStyled = styled.div`
+  position: relative;
+  width: calc(100%);
+  margin: 2rem auto;
+
+  @media (min-width: 768px) {
+    width: calc(50% - 4rem);
+    margin: 2rem;
+  }
+
+  @media (min-width: 1025px) {
+    width: calc(50% - 4rem);
+    margin: 3rem 2rem;
+  }
+
+  .image {
+    width: 100%;
+  }
+
+  .title {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    padding: 2rem;
+
+    &__content {
+      position: relative;
+      z-index: 10;
+
+      h2 {
+        ${B2OpenSansWhite};
+      }
+    }
+
+    &__button {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transition: all 0.4s ease-out;
+      transform: translate(-50%, 100%);
+      opacity: 0;
+      z-index: 10;
+
+      button {
+        ${buttonOneClear};
+      }
+    }
+
+    &__background {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      transition: all 0.4s ease-out;
+      background-color: rgba(55, 93, 119, 0.4);
+      z-index: 5;
+    }
+  }
+
+  &:hover {
+    .title__button {
+      transform: translate(-50%, -50%);
+      opacity: 1;
+    }
+    .title__background {
+      background-color: rgba(55, 93, 119, 0.8);
+    }
+  }
+`
+
+const SeriesItem = ({ item }) => {
+  return (
+    <SeriesItemStyled key={item.node.wordpress_id}>
+      <Link to={`/series/${item.node.slug}`}>
+        <div className="image">
+          <Img
+            fluid={
+              item.node.acf._wfc_mescat_featured_image.localFile.childImageSharp
+                .fluid
+            }
+            alt={item.node.name}
+          />
+        </div>
+        <div className="title">
+          <div className="title__content">
+            <h2>{item.node.name}</h2>
+          </div>
+          <div className="title__button">
+            <button>View Series</button>
+          </div>
+          <div className="title__background" />
+        </div>
+      </Link>
+    </SeriesItemStyled>
+  )
+}
+
+export default SeriesItem
