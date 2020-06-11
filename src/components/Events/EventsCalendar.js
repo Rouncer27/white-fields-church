f9a677199c23a0d0c43c3c1b8b595b2dbcd2fd3f
Rouncer27 <trevor@switchbackcreative.ca>
2020-06-11 14:05:26 -0600

    new calendar

--- src/components/Events/EventsCalendar.js
+++ src/components/Events/EventsCalendar.js
@@ -18,9 +18,9 @@ const EventsCalendar = () => {
       <div className="wrapper">
         <div className="calContainer">
           <iframe
-            src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FEdmonton&amp;src=ZXRyb3Q1bGtja2cxZDFlcGV2N3FpZzFicHNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23D50000"
-            width="100%"
-            height="700"
+            src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23deb46e&amp;ctz=America%2FEdmonton&amp;src=ZXRyb3Q1bGtja2cxZDFlcGV2N3FpZzFicHNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23D50000&amp;showNav=1&amp;showDate=1&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0"
+            width="800"
+            height="600"
             frameborder="0"
             scrolling="no"
           ></iframe>
