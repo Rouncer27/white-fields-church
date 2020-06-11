4d868b9e8d13e36d7e39d15005b79384c95fe665
Rouncer27 <trevor@switchbackcreative.ca>
2020-06-11 13:48:33 -0600

    calendar size

--- src/components/Events/EventsCalendar.js
+++ src/components/Events/EventsCalendar.js
@@ -6,17 +6,21 @@ const EventsCalendarSection = styled.section`
   .wrapper {
     ${standardWrapper};
   }
+
+  .calContainer {
+    width: 100%;
+  }
 `
 
 const EventsCalendar = () => {
   return (
     <EventsCalendarSection>
       <div className="wrapper">
-        <div>
+        <div className="calContainer">
           <iframe
             src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FEdmonton&amp;src=ZXRyb3Q1bGtja2cxZDFlcGV2N3FpZzFicHNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23D50000"
-            width="800"
-            height="600"
+            width="100%"
+            height="700"
             frameborder="0"
             scrolling="no"
           ></iframe>
