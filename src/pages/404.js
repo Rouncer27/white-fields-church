85748875a2b7bf0d0d4b2adf0b1962296f80c771
Rouncer27 <trevor@switchbackcreative.ca>
2020-07-16 10:32:25 -0600

    404 page fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,41 @@
 import React from "react"
+import styled from "styled-components"
+import { Link } from "gatsby"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import { standardWrapper, buttonOneBlue } from "../styles/helpers"
+
+const Main404 = styled.section`
+  .wrapper {
+    ${standardWrapper};
+    padding: 10rem 2rem;
+  }
+
+  div {
+    width: 100%;
+    text-align: center;
+  }
+
+  a {
+    ${buttonOneBlue};
+  }
+`
 
 const NotFoundPage = ({ location }) => (
   <Layout location={location}>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Main404>
+      <div className="wrapper">
+        <div>
+          <h1>NOT FOUND</h1>
+        </div>
+        <div>
+          <p>You just hit a page that doesn&#39;t exist.</p>
+          <Link to="/">Home</Link>
+        </div>
+      </div>
+    </Main404>
   </Layout>
 )
 
